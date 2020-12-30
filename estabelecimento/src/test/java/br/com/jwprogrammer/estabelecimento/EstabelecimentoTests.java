72c475d6253f08c8434b3a36c1e47c8d59a8d810
Josias Wando Nobre de queiroz <josiasn.queiroz@gmail.com>
2020-12-30 12:27:51 -0300

    Mudanças e readme da aplicaçao prontas para a primeira versao

--- estabelecimento/src/test/java/br/com/jwprogrammer/estabelecimento/EstabelecimentoTests.java
+++ estabelecimento/src/test/java/br/com/jwprogrammer/estabelecimento/EstabelecimentoTests.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import org.hibernate.ObjectNotFoundException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
